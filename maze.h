f582d68d1cfe7c2600c13e5c92a631f36cbd6c2f
Stefan Baron von Vietinghoff-Scheel <c15stefan.baronvonvietinghoffscheel@usafa.edu>
2013-12-11 16:03:45 -0700

    Header file where functions are declared for robot maze. So far not tested.

---
+++ maze.h
@@ -0,0 +1,16 @@
+/*
+ * maze.h
+ *
+ *  Created on: Dec 11, 2013
+ *      Author: C15Stefan.BaronvonVi
+ */
+
+#include <msp430g2553.h>
+
+#ifndef MAZE_H_
+#define MAZE_H_
+
+
+
+
+#endif /* MAZE_H_ */
