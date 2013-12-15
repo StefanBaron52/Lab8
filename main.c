ee6b17dfbe5dbf6d70550ce7c72eac90a39e256c
Stefan Baron von Vietinghoff-Scheel <c15stefan.baronvonvietinghoffscheel@usafa.edu>
2013-12-15 13:23:16 -0700

    This part of code is basically the heart of the robot maze movement. Here we take files which were already created from the previous two labs and instead of the sensor sending information to the LEDs to turn on, it sends information to the motors to start moving given a certain sensor reading.

--- main.c
+++ main.c
@@ -4,11 +4,53 @@
  * main.c
  */
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+
+	initADC10();
+	InitPinsOut();
+	ConfigTimer();
+
+	P1DIR |= BIT0|BIT6;
+
+	while(1) {
+
+		unsigned int leftVal = leftsensor(), centerVal = centersensor();
+
+
+		if (centerVal > 0x200){
+			MoveForward();
+			_delay_cycles(10000);
+			P1OUT |= BIT0;
+		}
+		else if (leftVal > 0x250){
+			MoveLeftMotorForwardSmall();
+			MoveRightMotorBackward();
+			_delay_cycles(10000);
+		}
+		else{
+			MoveBackward();
+			_delay_cycles(10000);
+
+
+			P1OUT &= ~BIT0;
+		}
+
+
+		/*
+		if (centerVal > 0x2ff)
+			P1OUT |= BIT0;
+		else
+			P1OUT &= ~BIT0;
+
+		if (leftVal > 0x150)
+			P1OUT |= BIT6;
+		else
+			P1OUT &= ~BIT6;
+			*/
+
+}
 
 
 
-
-	
 	return 0;
 }
